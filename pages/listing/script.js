d42468cb8d5516f654fdbbe4e79e250987090533
Titi <tytti_x@yahoo.com>
2025-04-24 15:14:43 +0300

    Products page

--- pages/listing/script.js
+++ pages/listing/script.js
@@ -1,11 +1,18 @@
-const products = [{ id: 1, title: "Phone", img: "src", price: 10 }];
+import { products } from "./products.js";
+
+const productElement = document.querySelector(".js-products");
+
+const addEventListeners = (container) => {
+	const buttons = container.querySelectorAll("button");
+	[...buttons].forEach((button) => button.addEventListener("click", () => {}));
+};
 
 const renderProduct = (product) => {
-	return `<div>
-    <img src="${product.img}">
-    <h4 class="product-tile">${product.title}</h4>
-    <p><strong>${product.price} Lei</strong></p>
-    <button data-id="${product.id}">Adauga in Cos</button>
+	return `<div class="prod-container">
+        <img src="${product.img}" alt="${product.title}">
+        <h2 class="prod-name">${product.title}</h2>
+        <div class="price">${product.price} Lei</div>
+        <button class="add-to-cart" data-id="${product.id}"><span>Adauga in Cos</span></button>
     </div>`;
 };
 
@@ -14,4 +21,11 @@ const renderProducts = () => {
 	products.forEach((product) => {
 		html = html + renderProduct(product);
 	});
+
+	productElement.innerHTML = html;
+	addEventListeners(productElement);
 };
+// exemplu storage pt cos clienti
+window.localStorage.setItem("app-cart-products", JSON.stringify([{ userId: 1, products: [1, 2, 3] }]));
+
+renderProducts();
